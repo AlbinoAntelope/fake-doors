22ef7a7ea80143f7ffa780cb4b6364b1c0cf974e
AlbinoAntelope <daniel.eryavec@sympatico.ca>
2016-11-20 17:33:35 -0500

    should run on heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
 // set the view engine to ejs
 app.set('views', process.cwd() + '/public/views');
 app.set('view engine', 'ejs');
@@ -20,5 +21,7 @@ app.get('/about', function(req, res) {
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(8080);
-console.log('8080 is the magic port');
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
